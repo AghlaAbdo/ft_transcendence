46f6c5e638fdd5ffb848da5b2a290c167adb153f
Abdellatif Aghla <aaghla@student.1337.ma>
2025-08-29 15:34:28 +0100

    read environement variables from .env

--- services/game-service/src/config/env.ts
+++ services/game-service/src/config/env.ts
@@ -3,8 +3,12 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const DB_NAME = process.env.DB_NAME || 'dataBase';
+const FASTIFY_PORT = parseInt(process.env.FASTIFY_PORT || '4000');
+const SOCKETIO_PORT = parseInt(process.env.SOCKETIO_PORT || '4040');
 
 
 export {
-    DB_NAME
+    DB_NAME,
+    FASTIFY_PORT,
+    SOCKETIO_PORT,
 }
