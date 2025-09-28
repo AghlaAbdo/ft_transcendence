056c8dc050e5cf9dcadbf2d32c9ce7fdde9c98e0
Ayoub Ben Chafai <aben-cha@student.1337.ma>
2025-09-28 15:50:39 +0100

    handle forgot/reset Password frontend/backend

--- frontend/src/app/(auth)/resetPassword/page.tsx
+++ frontend/src/app/(auth)/resetPassword/page.tsx
@@ -6,14 +6,41 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import React, { FormEvent, useEffect, useState } from 'react'
 import { toast } from 'sonner';
 
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const resetPasswordSchema = z
+  .object({
+    newPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(/[A-Z]/, "Must contain an uppercase letter")
+      .regex(/[a-z]/, "Must contain a lowercase letter")
+      .regex(/[0-9]/, "Must contain a number")
+      .regex(/[^A-Za-z0-9]/, "Must contain a special character"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+});
+
+
+
+type ResetPasswordInput = z.infer<typeof resetPasswordSchema>;
+
+
 const ResetPasswordPage = () => {
   const searchParams = useSearchParams();
   const [password, setPassword] = useState<string>("");
   const [confirm, setConfirm] = useState<string>("");
-//   let token: string | null;
   const [token, setToken] = useState<string | null>(null);
   
-  
+  const { register, handleSubmit, formState: { errors } } = useForm<ResetPasswordInput>({
+    resolver: zodResolver(resetPasswordSchema),
+  });
   const router = useRouter();
   
   useEffect(() => {
@@ -24,24 +51,24 @@ const ResetPasswordPage = () => {
     }
   }, [router]);
 
-  const handleResetPassword = async (e: FormEvent<HTMLFormElement>) =>{
-    e.preventDefault();
+  const handleResetPassword = async (data: ResetPasswordInput) =>{
+    // e.preventDefault();
     
-    if (password !== confirm) {
-        toast.error("❌ Passwords do not match");
-        return;
-    }
+    // if (password !== confirm) {
+    //     toast.error("❌ Passwords do not match");
+    //     return;
+    // }
 
-    if (!token) {
-        toast.error("❌ Missing token.");
-        return;
-    }
+    // if (!token) {
+    //     toast.error("❌ Missing token.");
+    //     return;
+    // }
 
     try {
       const response = await fetch("https://localhost:8080/api/auth/reset-password", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ token, newPassword: password }),
+          body: JSON.stringify({ token, newPassword: data.newPassword }),
           credentials: "include",
       });
 
@@ -62,26 +89,34 @@ const ResetPasswordPage = () => {
       <div className="bg-slate-800/80 backdrop-blur-xl rounded-3xl shadow-2xl p-8 w-full max-w-md border border-slate-700">
         <div className='text-3xl font-bold text-center mb-5'>Reset Your Password</div>
 
-        <form className='space-y-4' onSubmit={handleResetPassword}>         
+        <form className='space-y-4' onSubmit={handleSubmit(handleResetPassword)}>         
             <Input
               icon={Lock}
               type="password"
               placeholder="New password"
-              value={password}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+              {...register("newPassword")}
+            //   value={password}
+            //   onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
             />
+            {errors.newPassword && (
+                <p className="text-red-500 text-sm">{errors.newPassword.message}</p>
+            )}
             <Input
               icon={Lock}
               type="password"
               placeholder="Confirm new password"
-              value={confirm}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setConfirm(e.target.value)}
+              {...register("confirmPassword")}
+            //   value={confirm}
+            //   onChange={(e: React.ChangeEvent<HTMLInputElement>) => setConfirm(e.target.value)}
             />
-            {confirm && confirm !== password && (
+            {errors.confirmPassword && (
+                <p className="text-red-500 text-sm">{errors.confirmPassword.message}</p>
+            )}
+            {/* {confirm && confirm !== password && (
                 <p className="text-red-500 text-sm mt-1">
                     ❌ Passwords do not match
                 </p>)
-            }
+            } */}
             <button 
                 type='submit'
                 className='flex justify-center items-center border-2 border-slate-700
