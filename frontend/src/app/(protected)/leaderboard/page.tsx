b580ab42c85c0f4f68d6cdf0da64e2c2ef458246
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2025-10-03 10:40:39 +0000

    make leaderboard SSR component

--- frontend/src/app/(protected)/leaderboard/page.tsx
+++ frontend/src/app/(protected)/leaderboard/page.tsx
@@ -1,78 +1,20 @@
-'use client'
-
-import Badge from "@/components/leaderboard/Badge";
-import { get_leaderboard } from "../lib/leaderboard";
-import Table from "@/components/leaderboard/Table";
-import { use, useEffect, useState } from "react";
+import LeaderboardClient from "@/components/leaderboard/leaderboard";
 import { Player } from "@/constants/leaderboard";
-export default function Leaderboard({
+import { get_leaderboard } from "@/app/(protected)/lib/leaderboard";
+
+export default async function Leaderboard({
     searchParams,
 }: {
-    searchParams: Promise<{ page?: string }>
+    searchParams: { page?: string }
 }) {
-    const resolvedSearchParams = use(searchParams)
-    const [players, setPlayers] = useState<Player[]>([])
-    const [topPlayers, setTopPlayers] = useState<Player[]>([])
-    const [loading, setLoading] = useState(true)
-    const widthMap: Map<number, number> = new Map()
-    widthMap.set(1, 30)
-    widthMap.set(2, 30)
-    widthMap.set(3, 50)
     const limit = 20
-    const page: number = Number(resolvedSearchParams.page) || 1
+    const page: number = Number(searchParams?.page) || 1
     const numOfPlayers = 50
     const numOfPages = Math.ceil(numOfPlayers / limit)
     const offset = (page - 1) * limit
-    useEffect(() => {
-        async function getLeaderboard() {
-            const players = await get_leaderboard(page, offset, false)
-            const topPlayers = await get_leaderboard(page, offset, true)
-            setPlayers(players)
-            setTopPlayers(topPlayers)
-            setLoading(false)
-        }
-        getLeaderboard()
-    }, [])
-    useEffect(() => {
-        async function getLeaderboard() {
-            const players = await get_leaderboard(page, offset, false)
-            console.log(players)
-            setPlayers(players)
-        }
-        getLeaderboard()
-    }, [page])
+    const players: Player[] = await get_leaderboard(page, offset, false)
+    const topPlayers: Player[] = await get_leaderboard(page, offset, true)
     return (
-        loading ? (
-            <div className="flex justify-center items-center h-[100vh]">
-                <div className="animate-spin rounded-full h-15 w-15 border-b-2 border-blue-500 2xl:h-30 2xl:w-30 2xl:border-b-4"></div>
-            </div>
-        ) : (
-
-            <div className="w-[90%] m-auto">
-                <h1 className="text-center text-[2rem] mb-5">Leaderboard</h1>
-                <div className={`flex items-end w-[${widthMap.get(topPlayers.length)}%] mx-auto`}>
-
-                    {
-                        numOfPlayers > 1 ? (
-                            <Badge color={"orange-500"} showCrown={false} imgSrc={"/avatars/avatar2.png"} username={topPlayers[1].username} score={topPlayers[1].score} rank={2} />
-                        ) :
-                            (<></>)
-                    }
-                    {
-                        numOfPlayers > 0 ? (
-                            <Badge color={"yellow-500"} showCrown={true} imgSrc={"/avatars/avatar1.png"} username={topPlayers[0].username} score={topPlayers[0].score} rank={1} />
-                        ) :
-                            (<></>)
-                    }
-                    {
-                        numOfPlayers > 2 ? (
-                            <Badge color={"gray-500"} showCrown={false} imgSrc={"/avatars/avatar4.png"} username={topPlayers[2].username} score={topPlayers[2].score} rank={3} />
-                        ) :
-                            (<></>)
-                    }
-                </div>
-                <Table players={players} numOfPages={numOfPages} page={page} />
-            </div>
-        )
+        <LeaderboardClient players={players} topPlayers={topPlayers} numOfPlayers={numOfPlayers} page={page} numOfPages={numOfPages}/>
     )
 }
--- frontend/src/app/(protected)/lib/leaderboard.ts
+++ frontend/src/app/(protected)/lib/leaderboard.ts
@@ -4,11 +4,7 @@ import { Players } from "@/mocks/leaderboard";
 
 const is_mock = true
 
-export async function get_leaderboard(page, offset, onlyTop): Promise<Player[]> {
-   function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms))
-    }
-    await sleep(500)
+export function get_leaderboard(page, offset, onlyTop): Promise<Player[]> {
     const sorted = Players.sort((a, b) => b.score - a.score)
     if (onlyTop){
       return sorted.slice(0, 3)  
